834273b1267df1499e574ba918090894ad732de1
Tatiana Gurinovich <72029335+nafaniaa@users.noreply.github.com>
2023-11-13 16:28:02 +0300

    Add files via upload

---
+++ lab1_lin_task2/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void signalHandler(int signal) {
+	if (signal == SIGINT) {
+		printf("\nSIGINT %d", signal);
+	}
+	else if (signal == SIGTERM)
+	{
+		printf("\nSIGTERM %d", signal);
+	}
+	else {
+		printf("\n%d", signal);
+	}
+	exit(0);
+}
+
+void* printRandomNumber(void*) {
+	while (TRUE) {
+		printf("%f\n", (double)rand() / RAND_MAX);
+		sleep(3);
+	}
+}
+
+void* reversString(void*) {
+	while (TRUE) {
+		char str[250];
+		scanf("%s", str);
+		for (int i = strlen(str) - 1; i >= 0; --i) {
+			printf("%c", str[i]);
+		}
+		printf("\n");
+	}
+}
+
+int main() {
+	pthread_t thread1;
+	pthread_t thread2;
+	int first;
+	int second;
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+	first = pthread_create(&thread1, NULL, printRandomNumber, NULL);
+	second = pthread_create(&thread2, NULL, reversString, NULL);
+	while (TRUE) {}
+	return 0;
+}
