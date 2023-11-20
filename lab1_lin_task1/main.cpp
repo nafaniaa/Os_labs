c36eaa83dd56c24ff7770f412b895e168741ad06
Tatiana Gurinovich <72029335+nafaniaa@users.noreply.github.com>
2023-11-20 10:22:34 +0300

    Update main.cpp

--- lab1_lin_task1/main.cpp
+++ lab1_lin_task1/main.cpp
@@ -1,29 +1,37 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
 
+// Обработчик сигналов
 void signalHandler(int signal)
 {
-	if (signal == SIGINT) {
-		printf("\nSIGINT %d\n", signalno);
-		break;
-	}
-	else if (signal == SIGTERM) {
-		printf("\nSIGTERM %d\n", signalno);
-		break;
-	}
-	else {
-		printf("\n Signal\n");
-		break;
-	}
-
-	exit(0);
+  // Обработка сигнала SIGINT (Ctrl+C)
+  if (signal == SIGINT) {
+    printf("\nSIGINT %d\n", signal);
+    exit(0); // Завершение программы
+	  // или break?
+  }
+  // Обработка сигнала SIGTERM
+  else if (signal == SIGTERM) {
+    printf("\nSIGTERM %d\n", signal);
+    exit(0);  // Завершение программы
+  }
+  // Если получен другой сигнал, выводится общее сообщение
+  else {
+    printf("\nSignal\n");
+    exit(0);  // Завершение программы
+  }
 }
 
 int main(int argc, char** argv)
 {
-	signal(SIGINT, signalHandler);
-	signal(SIGTERM, signalHandler);
-	while (true);
-	return 0;
+  // Установка обработчика сигнала SIGINT
+  signal(SIGINT, signalHandler);
+
+  // Установка обработчика сигнала SIGTERM
+  signal(SIGTERM, signalHandler);
+
+  // Бесконечный цикл ожидания сигналов
+  while (true);
+  return 0;
 }
